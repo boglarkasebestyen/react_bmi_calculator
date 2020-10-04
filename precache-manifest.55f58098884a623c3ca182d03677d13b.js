f14723b7f1d976025487f18be463d1eb2a25927f
Boglárka Sebestyén <sebestyenboglarkacsilla@gmail.com>
2020-10-04 11:30:51 -0700

    Updates

---
+++ precache-manifest.55f58098884a623c3ca182d03677d13b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358519ec65d47a8174db316c9125f7a4",
+    "url": "/react_bmi_calculator_hooks/index.html"
+  },
+  {
+    "revision": "af7e604b4d68c766ea46",
+    "url": "/react_bmi_calculator_hooks/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "1a0e0c9c2e63fc74f054",
+    "url": "/react_bmi_calculator_hooks/static/css/main.3bf26044.chunk.css"
+  },
+  {
+    "revision": "af7e604b4d68c766ea46",
+    "url": "/react_bmi_calculator_hooks/static/js/2.4b7108ee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_bmi_calculator_hooks/static/js/2.4b7108ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0e0c9c2e63fc74f054",
+    "url": "/react_bmi_calculator_hooks/static/js/main.4998ad5b.chunk.js"
+  },
+  {
+    "revision": "fefe1cf1d04cc587eeee",
+    "url": "/react_bmi_calculator_hooks/static/js/runtime-main.43171eaa.js"
+  }
+]);
